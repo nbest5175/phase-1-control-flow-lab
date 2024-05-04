9bbade1227a378cee15228fe80afcdae6baa6dd0
Nicholas <bestnicholas05@gmail.com>
2024-05-04 14:15:26 -0400

    Completed Control Flow Lab

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride <= 400) {
+    return ('This one is on me!')
+  }
+  if (ride > 400 && ride < 2000) {
+    return ('That will be twenty bucks.')
+  }
+  if (ride > 2000 && ride < 2500) {
+    return ('I will gladly take your thirty bucks.')
+  }
+  if (ride > 2500) {
+    return ('No can do.')
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC'? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return ('Thank you so much.')
+      break;
+    case 'not as generous':
+      return ('Thank you.')
+      break;
+    case 'thanks for everything':
+      return ('Bye.')
+  }
 }
